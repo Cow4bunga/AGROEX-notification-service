7ce0b65968143ab397af716a035dfae98c4965bb
Ivan Levitsky <yavanyaprivet@gmail.com>
2024-05-27 23:10:48 +0300

    Added configuration for SQS

--- src/main/java/com/ventionteams/agroexp_notification_service/config/SQSConfig.java
+++ src/main/java/com/ventionteams/agroexp_notification_service/config/SQSConfig.java
@@ -1,6 +1,25 @@
 package com.ventionteams.agroexp_notification_service.config;
 
+import com.ventionteams.agroexp_notification_service.config.properties.SQSProperties;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 @Configuration
-public class SQSConfig {}
+public class SQSConfig {
+  @Bean
+  SqsAsyncClient sqsAsyncClient(SQSProperties sqsProperties) {
+    return SqsAsyncClient.builder()
+        .region(Region.of(sqsProperties.getRegion()))
+        .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+        .build();
+  }
+
+  @Bean
+  public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient) {
+    return SqsTemplate.builder().sqsAsyncClient(sqsAsyncClient).build();
+  }
+}
--- src/main/java/com/ventionteams/agroexp_notification_service/config/properties/SQSProperties.java
+++ src/main/java/com/ventionteams/agroexp_notification_service/config/properties/SQSProperties.java
@@ -1,10 +1,15 @@
 package com.ventionteams.agroexp_notification_service.config.properties;
 
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Data
-@ConfigurationProperties(prefix = "aws.sqs")
+@ConfigurationProperties(prefix = "spring.cloud.aws.sqs")
 @Component
-public class SQSProperties {}
+public class SQSProperties {
+  private String accessKey;
+  private String secretKey;
+  private String region;
+}
